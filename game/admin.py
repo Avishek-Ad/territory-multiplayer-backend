3fabbc47c2173d19139f92c0c30573c080336309
Avishek-Ad <adhikariavishek03@gmail.com>
2026-06-21 18:41:04 +0545

    Refactor admin registration for User and Room models; add inline configurations for UserProfile and RoomMember

--- game/admin.py
+++ game/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Room, RoomMember, Match, MatchResult
 
-admin.site.register(Room)
-admin.site.register(RoomMember)
+# admin.site.register(Room)
+# admin.site.register(RoomMember)
+
+class RoomMemberInline(admin.TabularInline):
+    model = RoomMember
+    extra = 1
+    fields = [
+        'user',
+        'is_host'
+    ]
+    
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ["room_code"]
+    inlines = [RoomMemberInline]
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import User, UserProfile
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+# admin.site.register(User)
+# admin.site.register(UserProfile)
+
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+    max_num = 1
+    extra = 1
+    fields = [
+        'avatar',
+        'rating',
+        'total_matches',
+        'wins'
+    ]
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [UserProfileInline]
+    
